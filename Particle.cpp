282ca5cfff9dfb7543c4cec6d964000fe120728e
jmeredith <jm.4444@icloud.com>
2026-01-21 16:10:40 -0600

    Adds a class constructor.

--- Particle.cpp
+++ Particle.cpp
@@ -1,8 +1,14 @@
 #include "Particle.h"
 
+Particle::Particle(int initial_x_position, int intitial_y_position, int initial_velocity) {
+    x_position = initial_x_position;
+    y_position = intitial_y_position;
+    velocity = initial_velocity;
+}
+
 void Particle::setXPosition(int new_x_position) {
     x_position = new_x_position;
-};
+}
 
 void Particle::setYPosition(int new_y_position) {
     y_position = new_y_position;
--- Particle.h
+++ Particle.h
@@ -1,5 +1,6 @@
 class Particle {
     public:
+        Particle(int initial_x_position, int intitial_y_position, int initial_velocity);
         void setXPosition(int new_x_position);
         void setYPosition(int new_y_position);
         void setVelocity(int new_velocity);
