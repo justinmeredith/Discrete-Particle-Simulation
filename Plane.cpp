35599ee6eaff8bb670382cb293f4eecad6c32d33
jmeredith <jm.4444@icloud.com>
2026-01-22 11:39:25 -0600

    Adds constructors and a method for displaying the plane.

--- Plane.cpp
+++ Plane.cpp
@@ -1,22 +1,50 @@
 #include "Plane.h"
+#include <iostream>
+#include <string>
 
+using std::cout;
+using std::endl;
+using std::string;
+
+// The class that serves as a grid or space for the particles to exist in
+
+// Constructor with arguments
 Plane::Plane(int initial_x_axis, int initial_y_axis) {
     x_axis = initial_x_axis;
     y_axis = initial_y_axis;
 }
 
-void Plane::set_x_axis(int new_x_axis) {
+// Constructor without arguments
+Plane::Plane() {
+    x_axis = 20;
+    y_axis = 20;
+}
+
+void Plane::setXAxis(int new_x_axis) {
     x_axis = new_x_axis;
 }
 
-void Plane::set_y_axis(int new_y_axis) {
+void Plane::setYAxis(int new_y_axis) {
     y_axis = new_y_axis;
 }
 
-int Plane::get_x_axis() {
+int Plane::getXAxis() {
     return x_axis;
 }
 
-int Plane::get_y_axis() {
+int Plane::getYAxis() {
     return y_axis;
+}
+
+void Plane::displayPlane() {
+    string printable_x_axis;
+    // Creates an x-axis of spaces
+    for (int i = 0; i < x_axis; ++i) {
+        printable_x_axis += "  ";
+    }
+
+    // Prints the pane 
+    for (int i = 0; i < y_axis; ++i) {
+        cout << "[" << printable_x_axis << "]" << endl;
+    }
 }
--- Plane.h
+++ Plane.h
@@ -1,10 +1,12 @@
 class Plane {
     public:
         Plane(int initial_x_axis, int initial_y_axis);
-        void set_x_axis(int new_x_axis);
-        void set_y_axis(int new_y_axis);
-        int get_x_axis();
-        int get_y_axis();
+        Plane();
+        void setXAxis(int new_x_axis);
+        void setYAxis(int new_y_axis);
+        int getXAxis();
+        int getYAxis();
+        void displayPlane();
     private:
         int x_axis;
         int y_axis;
