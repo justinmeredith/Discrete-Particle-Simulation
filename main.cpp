c2368c06e36ee21fcd6ecd12ec2c0f9c5b5a9b30
justinmeredith <jm.4444@icloud.com>
2026-01-23 13:24:04 -0600

    Implements the new setParticleGrid() feature and displays the new particle_grid with a single particle in the correct location.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include "Particle.cpp"
 #include "Plane.cpp"
 
 int main() {
     Plane testing_plane;
-    Particle testing_particle(5, 5, 0);
+    Particle testing_particle(10, 10, 0);
     vector<Particle> all_particles;
     all_particles.push_back(testing_particle);
+    testing_plane.setParticleGrid(all_particles);
     
-    testing_plane.displayPlane(all_particles);
+    testing_plane.displayPlane();
     return 0;
 }
