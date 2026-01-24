ecf6a8149a2db089663cd3d546982056e1f80436
justinmeredith <jm.4444@icloud.com>
2026-01-24 17:02:54 -0600

    Updates functions to use the new Velocity struct and adds an updatePosition method that takes an int time_step and moves the particle accordingly.

--- Particle.cpp
+++ Particle.cpp
@@ -3,7 +3,7 @@
 // The class that creates particle objects
 
 // Constructor with arguments
-Particle::Particle(int initial_x_position, int intitial_y_position, int initial_velocity) {
+Particle::Particle(int initial_x_position, int intitial_y_position, Velocity initial_velocity) {
     x_position = initial_x_position;
     y_position = intitial_y_position;
     velocity = initial_velocity;
@@ -13,7 +13,6 @@ Particle::Particle(int initial_x_position, int intitial_y_position, int initial_
 Particle::Particle() {
     x_position = 0;
     y_position = 0;
-    velocity = 0;
 }
 
 void Particle::setXPosition(int new_x_position) {
@@ -24,7 +23,7 @@ void Particle::setYPosition(int new_y_position) {
     y_position = new_y_position;
 }
 
-void Particle::setVelocity(int new_velocity) {
+void Particle::setVelocity(Velocity new_velocity) {
     velocity = new_velocity;
 }
 
@@ -36,6 +35,11 @@ int Particle::getYPosition(){
     return y_position;
 }
 
-int Particle::getVelocity() {
+Velocity Particle::getVelocity() {
     return velocity;
+}
+
+void Particle::updatePosition(int time_step) {
+    x_position = x_position + (velocity.x_velocity * time_step);
+    y_position = y_position + (velocity.y_velocity * time_step);
 }
--- Particle.h
+++ Particle.h
@@ -1,15 +1,18 @@
+#include "Velocity.h"
+
 class Particle {
     public:
-        Particle(int initial_x_position, int intitial_y_position, int initial_velocity);
+        Particle(int initial_x_position, int intitial_y_position, Velocity initial_velocity);
         Particle();
         void setXPosition(int new_x_position);
         void setYPosition(int new_y_position);
-        void setVelocity(int new_velocity);
+        void setVelocity(Velocity new_velocity);
         int getXPosition();
         int getYPosition();
-        int getVelocity();
+        Velocity getVelocity();
+        void updatePosition(int time_step);
     private:
         int x_position;
         int y_position;
-        int velocity;
+        Velocity velocity;
 };
