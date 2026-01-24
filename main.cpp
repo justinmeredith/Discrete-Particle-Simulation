cce7bb1665f009f30b9b7b860a68d698202f1507
justinmeredith <jm.4444@icloud.com>
2026-01-24 17:01:42 -0600

    Updates functions to use the new Velocity struct. Also adds two time states for testing that the velocity feature is being updated correctly.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 
 int main() {
     Plane testing_plane;
-    Particle testing_particle(10, 10, 0);
+    Particle testing_particle(10, 3, Velocity(-2, 3));
     vector<Particle> all_particles;
+
+    // This is a very messy testing state, I only have this code here to verify that the velocity component is in basic functioning order
+    // A menu or file load-in is necessary to begin streamlining this sort of testing
+    // There are also no boundary checks to prevent particles from falling off of the grid
+
+    // Starting state
     all_particles.push_back(testing_particle);
     testing_plane.setParticleGrid(all_particles);
+    testing_plane.displayPlane();
     
+    cout << "- - - - - - - - - - - - - - - - - - - -" << endl;
+
+    // Second state
+    all_particles.at(0).updatePosition(2);
+    testing_plane.setParticleGrid(all_particles);
     testing_plane.displayPlane();
     return 0;
 }
